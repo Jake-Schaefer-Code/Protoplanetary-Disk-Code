aa25801f288911300f4735e831c58a80082775be
Jake <87781057+Snakej07@users.noreply.github.com>
2023-07-28 11:31:23 -0500

    Add files via upload

--- DiffEvoTorus.py
+++ DiffEvoTorus.py
@@ -238,7 +238,9 @@ def differential_evolution(converged, restarting, mutation = (0.5,1.0), P = 0.7,
         count += genNum
         count2 += restart(x, fx)
         x = (x - bmin)/brange
-        fx[count2-1:] = np.array([objective_fn(xi) for xi in (x*brange+bmin)[count2-1:]])
+        print(np.shape(fx[count2-1:]))
+        fx[count2-1:N+1] = np.array([objective_fn(xi) for xi in (x*brange+bmin)[count2-1:N+1]]) # TODO: something weird about this... fix later
+        # TODO: got cannot cast shape (0,) to shape (6,) or vice versa error
     elif restarting == False:
         fx = np.array([objective_fn(xi) for xi in x*brange+bmin])
         
@@ -311,9 +313,8 @@ def differential_evolution(converged, restarting, mutation = (0.5,1.0), P = 0.7,
 
 
 
-
-
 def findChi(modelPath, dataPath):
+    global count2
 
     modelFile = open(modelPath, 'r')
     modelText = modelFile.read()
@@ -326,13 +327,8 @@ def findChi(modelPath, dataPath):
     modelDataLines = modelLines[1:]
     dataLines = dataLines[3:]
 
-    modelLam = []
-    modelFlux = [] 
-
-    dataLam = []
-    dataFlux = []
-    error = []
-
+    modelLam, modelFlux = [], []
+    dataLam, dataFlux, error, names = [], [], [], []
 
     def getLine(point1, point2):
         return (point1, (point2[1]-point1[1])/(point2[0]-point1[0]))
@@ -351,6 +347,7 @@ def findChi(modelPath, dataPath):
         valueslist = dataLines[i].split(" ")
         if valueslist[3] != 'nan' and float(valueslist[3]) != 0 and float(valueslist[0])*10**6 != 4.35:
             dataLam.append(float(valueslist[0])*10**6)
+            names.append(valueslist[1])
             dataFlux.append(float(valueslist[2]))
             error.append(float(valueslist[3]))
         
@@ -363,13 +360,20 @@ def findChi(modelPath, dataPath):
     pointDict = {'x':[], 'y':[]}
     modelPointDict = {'x':[], 'y':[]}
 
-    for tuple in xyerrTuples:
-        pointDict['x'] += [tuple[0]]
-        pointDict['y'] += [tuple[1]]
+    for i in range(len(names)): # Creating a dictionary of the data points based on author
+        names[i] = names[i].split(":")[0]
+        if pointDict.get(names[i]) == None:
+            pointDict[names[i]] = [list(xyerrTuples[i])]
+        else:
+            pointDict[names[i]] += [list(xyerrTuples[i])]
 
     for tuple in modelTuples:
         modelPointDict['x'] += [tuple[0]]
         modelPointDict['y'] += [tuple[1]]
+
+    for tuple in xyerrTuples:
+        pointDict['x'] += [tuple[0]]
+        pointDict['y'] += [tuple[1]]
 
     count = 0
     for j in range(len(xyerrTuples)-1):
@@ -383,12 +387,10 @@ def findChi(modelPath, dataPath):
             listOfRanges.append((xmin, xmax, count))
             count += 1
 
-
     dictOfBins = {"nearIr":{ranges:[] for ranges in listOfRanges[:14]}, "midIr":{ranges:[] for ranges in listOfRanges[14:25]}, 
                     "farIr":{ranges:[] for ranges in listOfRanges[25:31]}, "micro":{ranges:[] for ranges in listOfRanges[31:]}}
 
-
-    count = 0
+    #count = 0
     for point in modelTuples:
         for i in range(len(listOfRanges)):
             valuerange = listOfRanges[i]
@@ -402,11 +404,12 @@ def findChi(modelPath, dataPath):
                 else:
                     dictOfBins["micro"][valuerange] += [(point, valuerange[2])]
 
-    nearIrChi = 0
-    midIrChi = 0
-    farIrChi = 0
-    microChi = 0
-
+    plt.figure(facecolor='#808080')
+    ax = plt.axes()
+    ax.set_facecolor("#404040")
+    plt.plot(modelPointDict['x'], modelPointDict['y'], color="white", alpha=0.75)
+    
+    nearIrChi, midIrChi, farIrChi, microChi = 0, 0, 0, 0
     for irBin in dictOfBins:
         for valuerange in dictOfBins[irBin]:
             valuesInBin = dictOfBins[irBin][valuerange]
@@ -429,11 +432,31 @@ def findChi(modelPath, dataPath):
                 else:
                     microChi += d_chi
 
+    # Weighting the Chi values based on the number of data points
     nearIrChi = (nearIrChi * len(listOfRanges)) / len(listOfRanges[:14])
     midIrChi = (midIrChi * len(listOfRanges)) / len(listOfRanges[14:25])
     farIrChi = (farIrChi * len(listOfRanges)) / len(listOfRanges[25:31])
     microChi = (microChi * len(listOfRanges)) / len(listOfRanges[31:])
     totalChi = nearIrChi + midIrChi + farIrChi + microChi
+
+    # Plotting the graph based on wavelength ranges
+    plt.plot([fitPts[i][0] for i in range(len(fitPts[:15]))],[fitPts[i][1] for i in range(len(fitPts[:15]))], color="#FFCC00", alpha=0.75)
+    plt.plot([fitPts[i+14][0] for i in range(len(fitPts[14:26]))],[fitPts[i+14][1] for i in range(len(fitPts[14:26]))], color="#FF6600", alpha=0.75)
+    plt.plot([fitPts[i+25][0] for i in range(len(fitPts[25:31]))],[fitPts[i+25][1] for i in range(len(fitPts[25:31]))], color="#FF3300", alpha=0.75)
+    plt.plot([fitPts[i+30][0] for i in range(len(fitPts[30:]))],[fitPts[i+30][1] for i in range(len(fitPts[30:]))], color="#CC0066", alpha=0.75)
+
+    # Plotting data points and error bars
+    for key in pointDict:
+        xVals, yVals, errVals = [],[],[]
+        for val in pointDict[key]:
+            xVals += [val[0]]
+            yVals += [val[1]]
+            errVals += [val[2]]
+        plt.scatter(xVals, yVals, marker="o", linewidths=0.5, label = str(key), alpha = 0.75)
+        plt.errorbar(xVals, yVals, yerr = errVals, fmt = 'None', ecolor='white', alpha=0.5)
+    
+    # Saving the figure
+    plt.savefig("sed"+str(count2)+".png")
 
     return totalChi
 
